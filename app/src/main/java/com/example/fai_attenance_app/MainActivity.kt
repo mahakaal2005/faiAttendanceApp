a02ee2671998635f320b9b94fe4a6d02af89dee7
Mahakaal5002 <atul.k.singh5002@gmail.com>
2024-12-16 00:49:00 +0530

    -m
    First Commit

---
+++ app/src/main/java/com/example/fai_attenance_app/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.shopping_app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+        val name:EditText = findViewById(R.id.name)
+
+        button.setOnClickListener {
+            startActivity(Intent(this,MainActivity::class.java).putExtra("name",name.text.toString()))
+            finish()
+        }
+    }
+}
